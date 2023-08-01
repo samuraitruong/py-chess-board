0f0af16adf283be0bbaa1a01df0951321d0ac1db
Truong Nguyen <truong.nguyen@tymlez.com>
2023-08-01 13:43:08 +1000

    revert try-cacth

--- pre_cache_images.py
+++ pre_cache_images.py
@@ -10,14 +10,10 @@ def cache_all():
 
     subfolders = [f.name for f in os.scandir(piece_set_folder) if f.is_dir()]
     for piece_set in subfolders:
-        try:
-            print('generate board with ' + piece_set)
-            board = Board()
-            board.theme.set_piece_set(piece_set)
-            board.generate()
-        # pylint: disable=W0718
-        except Exception:  # [broad-exception-caught]
-            print('Error generating board images: ' + piece_set)
+        print('generate board with ' + piece_set)
+        board = Board()
+        board.theme.set_piece_set(piece_set)
+        board.generate()
 
 
 if __name__ == '__main__':
